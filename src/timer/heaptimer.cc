305ba786573f32848912938e321622e0bde27080
willhunger <willhunger@foxmail.com>
2021-03-23 22:35:44 +0800

    Heap Timer

---
+++ src/timer/heaptimer.cc
@@ -0,0 +1,138 @@
+#include "heaptimer.h"
+
+void HeapTimer::swapNode(size_t i, size_t j)
+{
+    assert(i >= 0 && i < heap.size());
+    assert(j >= 0 && j < heap.size());
+    std::swap(heap[i], heap[j]);
+    ref[heap[i].id] = i;
+    ref[heap[j].id] = j;
+}
+
+void HeapTimer::heapifyUp(size_t i)
+{
+    assert(i >= 0 && i < heap.size());
+    size_t j = (i - 1) / 2;
+    while (j >= 0)
+    {
+        if (heap[j] < heap[i])
+            break;
+        swapNode(i, j);
+        i = j;
+        j = (i - 1) / 2;
+    }
+}
+
+bool HeapTimer::heapifyDown(size_t index, size_t n)
+{
+    assert(index >= 0 && index < heap.size());
+    assert(n >= 0 && n <= heap.size());
+    size_t i = index;
+    size_t j = i * 2 + 1;
+    while (j < n) 
+    {
+        if (j + 1 < n && heap[j + 1] < heap[j]) ++j;
+        if (heap[i] < heap[j]) break;
+        swapNode(i, j);
+        i = j;
+        j = i * 2 + 1;
+    }
+    return i > index;
+}
+
+
+void HeapTimer::remove(size_t index)
+{
+    assert(!heap.empty() && index >= 0 && index < heap.size());
+    size_t i = index;
+    size_t n = heap.size() - 1;
+    assert(i <= n);
+    if (i < n)
+    {
+        swapNode(i, n);
+        if (!heapifyDown(i, n))
+            heapifyUp(i);
+    }
+    ref.erase(heap.back().id);
+    heap.pop_back();
+}
+
+void HeapTimer::add(int id, int timeOut, const timeOutCallBack& callback)
+{
+    assert(id >= 0);
+    size_t i;
+    if (ref.find(id) == end(ref))
+    {
+        i = heap.size();
+        ref[id] = i;
+        heap.emplace_back(id, Clock::now() + millisecond(timeOut), callback);
+        heapifyUp(i);
+    }
+    else 
+    {
+        i = ref[id];
+        heap[i].expires = Clock::now() + millisecond(timeOut);
+        heap[i].callback = callback;
+        if (!heapifyDown(i, heap.size()))
+            heapifyUp(i);
+    }
+}
+
+
+void HeapTimer::doWork(int id)
+{
+    if (heap.empty() || ref.find(id) == end(ref))
+        return;
+    size_t i = ref[id];
+    auto node = heap[i];
+    node.callback();
+    remove(i);
+}
+
+void HeapTimer::tick()
+{
+    if (heap.empty())
+    {
+        return;
+    }
+    while (!heap.empty())
+    {
+        auto node = heap.front();
+        if (std::chrono::duration_cast<millisecond>(node.expires - Clock::now()).count() > 0)
+        {
+            break;
+        }
+        node.callback();
+        pop();
+    }
+}
+
+void HeapTimer::pop()
+{
+    assert(!heap.empty());
+    remove(0);
+}
+
+void HeapTimer::clear()
+{
+    ref.clear();
+    heap.clear();
+}
+
+int HeapTimer::getNextTick()
+{
+    tick();
+    size_t res = -1;
+    if (!heap.empty())
+    {
+        res = std::chrono::duration_cast<millisecond>(heap.front().expires - Clock::now()).count();
+        if (res < 0)
+            res = 0;
+    }
+    return res;
+}
+
+bool HeapTimer::empty()
+{
+    return heap.empty();
+}
---
+++ src/timer/heaptimer.h
@@ -0,0 +1,79 @@
+#ifndef HEAP_TIMER_H
+#define HEAP_TINMR_H
+
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+#include <functional>
+#include <chrono>
+#include <assert.h>
+#include <time.h>
+#include <arpa/inet.h>
+
+using timeOutCallBack = std::function<void()>;
+using Clock = std::chrono::high_resolution_clock;
+using millisecond = std::chrono::milliseconds;
+using timestamp = Clock::time_point;
+
+struct TimerNode 
+{   
+    TimerNode(int _id, timestamp _expires, timeOutCallBack _callback):
+        id(_id), expires(_expires), callback(_callback) {}
+        
+    bool operator< (const TimerNode& rhs);
+
+    int id;
+    timestamp expires;
+    timeOutCallBack callback;
+};
+
+bool TimerNode::operator< (const TimerNode& rhs)
+{
+    return this->expires < rhs.expires;
+}
+
+class HeapTimer
+{
+public:
+    HeapTimer()
+    {
+        heap.reserve(64);
+    }
+
+    ~HeapTimer()
+    {
+        clear();
+    }
+
+    void adjust(int id, int newExpires);
+
+    void add(int id, int timeOut, const timeOutCallBack& callback);
+
+    void doWork(int id);
+
+    void clear();
+
+    void tick();
+
+    void pop();
+
+    int getNextTick();
+
+    bool empty();
+    
+private:
+    void remove(size_t index);
+
+    void heapifyUp(size_t i);
+
+    bool heapifyDown(size_t index, size_t n);
+
+    void swapNode(size_t i, size_t j);
+
+    std::vector<TimerNode> heap;
+
+    std::unordered_map<int, size_t> ref;
+};
+
+#endif // !HEAP_TIMER_H
+
